2b6769f6be0475bf1dce18f5fd899c4c6cc1a576
Bora Ya Diul <borayadiul@Boras-MacBook-Pro.local>
2024-02-22 18:13:43 -0500

    Lab 3 work

---
+++ mywork/lab3/env-vars.py
@@ -0,0 +1,16 @@
+#!/Library/Frameworks/Python.framework/Versions/3.12/bin/python3
+
+
+
+import os
+
+
+os.environ["FAV_ANIMAL"] = input('What is your favorite animal? ')
+
+os.environ["STATE"] = input('What state are you from? ')
+
+os.environ["CLASS_NUM"] = input('How many classes are you taking this semester? ')
+
+print(os.getenv("FAV_ANIMAL"))
+print(os.getenv("STATE"))
+print(os.getenv("CLASS_NUM"))
---
+++ mywork/lab3/madlib.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+clear
+echo "Let's build a mad-lib!"
+
+read -p "1. Please give me an adjective: " ADJ1
+read -p "2. Please give me a noun: " N1
+read -p "3. Please give me a verb: " V1
+read -p "4. Please give me an adverb: " AV1
+read -p "5. Please give me a noun: " N2
+read -p "6. Please give me an adjective: " ADJ2
+read -p "7. Please give me an adverb: " AV2
+read -p "8. Please give me a verb: " V2
+
+echo "On a $ADJ1, hot June afternoon, $N1 was going on a hike when they unintentionally $V1 down a hill. 
+$N1 oriented themselves only to realize that they were covered in $ADJ2 bugs. The bugs were $AV1 crawling all over!
+To escape, $N1 $V2 until they encountered a $N2. The $N2 guided $N1 by $AV2 whispering in their ear 
+until $N1 was able to make it back up to the hike trail."
