573f2436c48e7e59bd8b4332ba2e2d8885b7b47d
Bora Ya Diul <borayadiul@Boras-MacBook-Pro.local>
2024-02-23 17:23:42 -0500

    reformatting

--- mywork/lab3/env-vars.py
+++ mywork/lab3/env-vars.py
@@ -4,11 +4,10 @@
 import os
 
 os.environ["FAV_ANIMAL"] = input('What is your favorite animal? ')
+print(os.getenv("FAV_ANIMAL"))
 
 os.environ["STATE"] = input('What state are you from? ')
+print(os.getenv("STATE")) 
 
 os.environ["CLASS_NUM"] = input('How many classes are you taking this semester? ')
-
-print(os.getenv("FAV_ANIMAL"))
-print(os.getenv("STATE"))
 print(os.getenv("CLASS_NUM"))
